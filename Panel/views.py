479c48fdb5297280746a729fb212386175d15492
Majid Hemmati <majid.hemmati@yahoo.com>
2022-08-13 00:44:17 +0430

    WareHouse

--- Panel/views.py
+++ Panel/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.views import View
+from django.http import JsonResponse
+from django.db import connection
+from rest_framework.decorators import api_view
+from django.http import HttpResponse
+
+import datetime
+import Infrastructure.ManageCookie
+import pyodbc
+@api_view(['POST', 'GET'])
+def AdminPanelindex(request):
+    context = {}
+    template_name = "AdminPanel_Page.html"
+
+    try:
+
+        return render(request, template_name, {'context': context})
+    except Exception as ex:
+        return render(request, template_name, context)
+
+
