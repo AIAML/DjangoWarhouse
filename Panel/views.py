c7a40c8c27fababd23627578561c7a3d892788fb
Majid Hemmati <majid.hemmati@yahoo.com>
2022-08-13 01:01:09 +0430

    WareHouse

--- Panel/views.py
+++ Panel/views.py
@@ -5,10 +5,10 @@ from django.http import JsonResponse
 from django.db import connection
 from rest_framework.decorators import api_view
 from django.http import HttpResponse
-
 import datetime
 import Infrastructure.ManageCookie
 import pyodbc
+
 @api_view(['POST', 'GET'])
 def AdminPanelindex(request):
     context = {}
